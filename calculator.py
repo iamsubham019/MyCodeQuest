374881fc99af0b1e0779e1eec36856d79e9c8a61
Subham Pal <iamsubham019@gmail.com>
2024-12-22 13:00:02 +0530

    Add files via upload
    A simple Python calculator that performs basic arithmetic operations like addition, subtraction, multiplication, and division. Perfect for beginners to understand Python basics and improve coding skills!

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#i am subham pal
+# i have created a calculator using the python commands 
+a=16
+b=4
+ans1=a+b
+print("the addition of ",a," and ",b," is ", ans1)
+ans2=a-b
+print("the substraction of ",a," and ",b," is ",ans2)
+ans3=a*b
+print("the multiplication of ",a," and ",b," is ",ans3)
+ans4=a/b
+print("the division of ",a," and ",b," is ",ans4)
+ans5=a**b
+print("the exponential of ",b," over ",a," is ",ans5)
